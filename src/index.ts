51ea1fe5be69aa07c065730cc21762877b34b5a0
Jennifer Fox <jenniferelizfox@gmail.com>
2025-01-22 21:08:15 +0000

    functions added: get all users, get a user, update a user, delete a user.

--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,68 @@ app.post("/users", async(req: Request, res: Response) => {
     );
 
     res.json(newUser.rows[0])
+
   } catch (err: any) {
     console.error(err.message);
     res.status(500).json({ error: err.message });
   }
 });
 
-// TODOS:
-//update a user
-//delete a user
-//get a user
+//get all users
+app.get("/users", async(req, res) => {
+  try {
+    const allUsers = await pool.query("SELECT * FROM users");
 
+    res.json(allUsers.rows)
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a user
+app.get("/users/:id", async(req, res) => {
+  const {id} = req.params
+  try {
+    const user = await pool.query("SELECT * FROM users WHERE user_id = $1",
+      [id]);
+
+      res.json(user.rows)
+
+  } catch (err) {
+    console.error(err.message);
+  }});
+
+//update a user
+app.put("/users/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, age } = req.body;
+
+    const updateUsers = await pool.query("UPDATE users SET name = $1, age = $2 WHERE user_id = $3",
+      [name, age, id]);
+
+      res.json("Users was updated")
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// TODOS:
+//delete a user
+app.delete("/users/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteUser = await pool.query("DELETE FROM users WHERE user_id = $1",
+      [id]);
+
+      res.json("User was successfully deleted")
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 // Questions for Alex:
 
