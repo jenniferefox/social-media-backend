cc7f48baa0029e2a8347bf84b547f902bd268601
Jennifer Fox <jenniferelizfox@gmail.com>
2025-01-26 22:06:45 +0000

    posts routes added and get/put/post/delete functions created

--- src/index.ts
+++ src/index.ts
@@ -40,8 +40,9 @@ app.get("/users", async(req, res) => {
 
 //get a user
 app.get("/users/:id", async(req, res) => {
-  const {id} = req.params
+
   try {
+    const {id} = req.params
     const user = await pool.query("SELECT * FROM users WHERE user_id = $1",
       [id]);
 
@@ -75,6 +76,135 @@ app.delete("/users/:id", async(req, res) => {
       [id]);
 
       res.json("User was successfully deleted")
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
+
+//create a post
+app.post("/users/:id/posts", async(req: Request, res: Response) => {
+  try {
+    const { title, content } = req.body;
+    const path = req.path;
+
+    // Regular expression to match a UUID
+    const regex = /\/users\/([a-f0-9\-]{36})\//;
+    const match = path.match(regex);
+
+    if (match && match[1]) {
+      console.log("Extracted UUID:", match[1]);
+      const user_id = match[1]
+      const newPost = await pool.query(
+      "INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3) RETURNING *",
+      [title, content, user_id]
+    );
+
+    res.json(newPost.rows[0])
+    } else {
+      console.log("No UUID found.");
+    }
+
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
+
+//get all posts
+app.get("/users/:id/posts", async(req, res) => {
+  try {
+    const path = req.path;
+    const regex = /\/users\/([a-f0-9\-]{36})\//;
+    const match = path.match(regex);
+
+    if (match && match[1]) {
+      console.log("Extracted UUID:", match[1]);
+      const user_id = match[1]
+      const allPosts = await pool.query("SELECT * FROM posts WHERE user_id = $1",
+        [user_id]
+      );
+      res.json(allPosts.rows)
+
+    } else {
+      console.log("No UUID found.");
+    }
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a post
+app.get("/users/:id/posts/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+
+    const path = req.path;
+    const regex = /\/users\/([a-f0-9\-]{36})\//;
+    const match = path.match(regex);
+
+    if (match && match[1]) {
+      console.log("Extracted UUID:", match[1]);
+      const user_id = match[1]
+      const {id} = req.params
+      const post = await pool.query("SELECT * FROM posts WHERE user_id = $1 AND id = $2 ",
+        [id, user_id]);
+
+      res.json(post.rows)
+    } else {
+    console.log("No UUID found.");}
+
+  } catch (err) {
+    console.error(err.message);
+  }});
+
+//update a post
+app.put("/users/:id/posts/:id", async(req, res) => {
+  try {
+
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const path = req.path;
+    const regex = /\/users\/([a-f0-9\-]{36})\//;
+    const match = path.match(regex);
+
+    if (match && match[1]) {
+      console.log("Extracted UUID:", match[1]);
+      const user_id = match[1]
+      const {id} = req.params
+      const updatePosts = await pool.query("UPDATE posts SET title = $1, content = $2 WHERE id = $3 AND user_id = $4",
+        [title, content, id, user_id]);
+
+    } else {
+    console.log("No UUID found.");}
+
+    res.json("Posts was updated")
+
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a post
+app.delete("/users/:id/posts/:id", async(req, res) => {
+  try {
+    const { id } = req.params;
+    const path = req.path;
+    const regex = /\/users\/([a-f0-9\-]{36})\//;
+    const match = path.match(regex);
+
+    if (match && match[1]) {
+      console.log("Extracted UUID:", match[1]);
+      const user_id = match[1]
+      const deletePost = await pool.query("DELETE FROM posts WHERE id = $1 AND user_id = $2",
+        [id, user_id]);
+
+    } else {
+    console.log("No UUID found.");}
+
+    res.json("Post was successfully deleted")
 
   } catch (err) {
     console.error(err.message)
