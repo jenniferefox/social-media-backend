f3774f0cfdbd1a943f64dce0209cfbf2e3e24153
Jennifer Fox <jenniferelizfox@gmail.com>
2025-01-28 10:57:33 +0000

    function to get user_id and check match located here

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+export function matchUserUUID(path: string) {
+  const regex = /\/users\/([a-f0-9\-]{36})\//;
+  const match = path.match(regex);
+
+  if (match && match[1]) {
+    console.log("Extracted UUID:", match[1]);
+    return match[1]
+
+  } else {
+    console.log("No UUID found.")
+    return;
+  };
+};
+
+// test:
+// const newPath = '/users/29b2d67a-c843-4743-863f-c756f1202aee/posts/'
+// console.log(matchUserUUID(newPath))
