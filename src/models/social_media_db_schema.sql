3b028312972c67f6f1c31790c114685ebd3ae922
Jennifer Fox <jenniferelizfox@gmail.com>
2025-01-29 18:58:00 +0000

    added likes, comments, friendships tables. Query added to get list of friends of a user

--- src/models/social_media_db_schema.sql
+++ src/models/social_media_db_schema.sql
@@ -17,3 +17,33 @@ CREATE TABLE posts (
   created_at TIMESTAMP DEFAULT NOW(),
   user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE
 );
+
+CREATE TABLE comments (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID,
+  content VARCHAR(150),
+  post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE likes (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id UUID,
+  created_at TIMESTAMP DEFAULT NOW(),
+  post_id UUID NOT NULL REFERENCES posts(id) ON DELETE CASCADE
+);
+
+CREATE TABLE friendships (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id1 UUID,
+  user_id2 UUID ,
+  created TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (user_id1) REFERENCES users(user_id),
+  FOREIGN KEY (user_id2) REFERENCES users(user_id)
+);
+
+
+SELECT * FROM users WHERE users.user_id IN (
+    (SELECT user_id1 FROM friendships WHERE user_id2 = '29b2d67a-c843-4743-863f-c756f1202aee')
+    UNION
+    (SELECT user_id2 FROM friendships WHERE user_id1 = '29b2d67a-c843-4743-863f-c756f1202aee')
+);
