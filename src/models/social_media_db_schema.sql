2ca6809ea2675d920190fa63cafb16fb9f8884d4
Jennifer Fox <jenniferelizfox@gmail.com>
2025-02-16 00:01:05 +0000

    users table redesigned, new fields added

--- src/models/social_media_db_schema.sql
+++ src/models/social_media_db_schema.sql
@@ -6,8 +6,12 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE TABLE users (
   user_id UUID PRIMARY KEY NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL
+  name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  password VARCHAR(100) NOT NULL,
+  favourite_animal VARCHAR(100) NOT NULL,
+  favourite_colour VARCHAR(100) NOT NULL,
+  favourite_snack VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE posts (
